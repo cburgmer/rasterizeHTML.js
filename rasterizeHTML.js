32e884e6d8ad2479b157f7d127be82b58369248f
Christoph Burgmer <cburgmer@ira.uka.de>
2012-06-15 00:37:17 +0200

    Fix Firefox workaround

--- rasterizeHTML.js
+++ rasterizeHTML.js
@@ -351,14 +351,20 @@ var rasterizeHTML = (function () {
 
     var workAroundFirefoxBugForInlinedImages = function (doc, svg) {
         // Firefox will not show an inlined background-image until the svg is connected to the DOM it seems.
-        var doNotGarbageCollect;
+        var workaroundId = "rasterizeHTML_js_FirefoxWorkaround",
+            doNotGarbageCollect;
 
         if (window.navigator.userAgent.indexOf("Firefox") >= 0) {
+            doNotGarbageCollect = doc.getElementById(workaroundId);
+            if (doNotGarbageCollect) {
+                doNotGarbageCollect.parentNode.removeChild(doNotGarbageCollect);
+            }
             doNotGarbageCollect = doc.createElement("div");
             doNotGarbageCollect.innerHTML = svg;
             doNotGarbageCollect.style.visibility = "hidden";
             doNotGarbageCollect.style.width = "0px";
             doNotGarbageCollect.style.height = "0px";
+            doNotGarbageCollect.id = "rasterizeHTML_js_FirefoxWorkaround";
             doc.getElementsByTagName("body")[0].appendChild(doNotGarbageCollect);
         }
     };
