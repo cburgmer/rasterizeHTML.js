334867c01cc518a6c142e7e5be376e50b8577b24
Christoph Burgmer <cburgmer@ira.uka.de>
2014-01-26 21:56:17 +0100

    Remove unused test helper code

--- test/helpers.js
+++ test/helpers.js
@@ -5,26 +5,21 @@ var isWebkit = navigator.userAgent.indexOf("WebKit") >= 0,
         if (! isWebkit) {
             return it(text, functionHandle);
         } else {
-            safeLog('Warning: "' + text + '" is disabled on this platform');
+            console.log('Warning: "' + text + '" is disabled on this platform');
         }
     },
     ifNotInPhantomJsIt = function(text, functionHandle) {
         if (! isPhantomJs) {
             return it(text, functionHandle);
         } else {
-            safeLog('Warning: "' + text + '" is disabled on this platform');
+            console.log('Warning: "' + text + '" is disabled on this platform');
         }
     },
     ifNotInPhantomJSAndNotLocalRunnerIt = function (text, functionHandle) {
         if (! isPhantomJs && ! isLocalRunner) {
             return it(text, functionHandle);
         } else {
-            safeLog('Warning: "' + text + '" is disabled on this platform');
-        }
-    },
-    safeLog = function (msg) {
-        if (window.console && console.log) {
-            console.log(msg);
+            console.log('Warning: "' + text + '" is disabled on this platform');
         }
     };
 
@@ -88,63 +83,12 @@ var rasterizeHTMLTestHelper = (function () {
         return doc;
     };
 
-    module.readFixturesOrFail = function (url) {
-        var content,
-            fixtureUrl = jasmine.getFixtures().fixturesPath + url;
-
-        $.ajax({
-            dataType: 'text',
-            url: fixtureUrl,
-            async: false,
-            cache: false,
-            success: function (theContent) {
-                content = theContent;
-            },
-            error: function () {
-                throw "unable to read fixture";
-            }
-        });
-
-        return content;
-    };
-
     module.readDocumentFixtureWithoutBaseURI = function (url) {
         var html = readFixtures(url),
             doc = document.implementation.createHTMLDocument("");
 
         doc.documentElement.innerHTML = html;
         return doc;
-    };
-
-    module.getLocalDocumentImage = function (image, finishHandler) {
-        var img = new window.Image();
-
-        img.onload = function () {
-            finishHandler(img);
-        };
-        img.src = image.attributes.src.nodeValue; // Chrome 19 sets image.src to ""
-    };
-
-    module.compareImageToReference = function (image, referenceImageId) {
-        var localImg = null;
-
-        // Gecko & Webkit won't allow direct comparison of images, need to get local first
-        runs(function () {
-            module.getLocalDocumentImage(image, function (img) { localImg = img; });
-        });
-
-        waitsFor(function () {
-            return localImg !== null;
-        }, "Move of image to local", 200);
-
-        runs(function () {
-            expect(localImg).toImageDiffEqual(window.document.getElementById(referenceImageId));
-        });
-    };
-
-    module.getBaseUri = function () {
-        // Strip of file part
-        return document.baseURI.replace(/\/[^\/]*$/, "/");
     };
 
     module.addStyleToDocument = function (doc, styleContent) {
@@ -154,15 +98,6 @@ var rasterizeHTMLTestHelper = (function () {
         styleNode.appendChild(doc.createTextNode(styleContent));
 
         doc.getElementsByTagName('head')[0].appendChild(styleNode);
-    };
-
-    var getImageForURL = function (url, finishHandler) {
-        var img = new window.Image();
-
-        img.onload = function () {
-            finishHandler(img);
-        };
-        img.src = url;
     };
 
     module.deleteAdditionalFieldsFromErrorsUnderPhantomJS = function (errors) {
