889e425eca9221b6912d87b91b088b462296e45d
Christoph Burgmer <cburgmer@ira.uka.de>
2014-03-29 20:45:24 +0100

    Revert "Another try at re-enabling the characterisation test against twitter page"
    This reverts commit e349ac917d8c5e806305378d8b8a17321675c44b.

--- test/inlineIntegration/runInlineTests.sh
+++ test/inlineIntegration/runInlineTests.sh
@@ -81,7 +81,8 @@ runTest() {
 
 main() {
     runTest "github" "github.com/index.html"
-    runTest "twitter" "twitter.com/index.html"
+    # Flaky, doesn't always terminate
+    #runTest "twitter" "twitter.com/index.html"
 
     if [[ "$failedTests" -eq 0 ]]; then
         echo "DONE"
