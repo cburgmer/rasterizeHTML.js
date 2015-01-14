cburgmer/rasterizeHTML.js

commit cfa04ba1dc633ff45abcf6334ac147ed5b73a1b5

Refactor, unify style selector rewrites

src/documentUtil.js
@@ -41,18 +41,27 @@ var documentUtil = (function () {
         styleElement.textContent = cssRulesToText(styleElement.sheet.cssRules);
     };
 
-    module.rewriteStyleRuleSelector = function (doc, oldSelector, newSelector) {
-        // Assume that oldSelector is always prepended with a ':' or '.' for now, so no special handling needed
-        var oldSelectorRegex = oldSelector + '(?=\\W|$)';
+    var matchingSimpleSelectorsRegex = function (simpleSelectorList) {
+        return '(' +
+            '(?:^|\\W)' +                  // start of string or non-alphabetic character,
+            '|' +                          // ... or ...
+            '(?=\\W)' +                    // the next character parsed is not an alphabetic character
+            ')' +
+            simpleSelectorList.join('|') + // one out of the given simple selectors
+            '(?=\\W|$)';                   // followed either by a non-alphabetic character or the end of the string
+    };
+
+    var replaceSimpleSelectorsBy = function (doc, simpleSelectorList, caseInsensitiveReplaceFunc) {
+        var selectorRegex = matchingSimpleSelectorsRegex(simpleSelectorList);
 
         asArray(doc.querySelectorAll('style')).forEach(function (styleElement) {
             var matchingRules = asArray(styleElement.sheet.cssRules).filter(function (rule) {
-                    return rule.selectorText && new RegExp(oldSelectorRegex).test(rule.selectorText);
-                });
+                return rule.selectorText && new RegExp(selectorRegex, 'i').test(rule.selectorText);
+            });
 
             if (matchingRules.length) {
                 matchingRules.forEach(function (rule) {
-                    var selector = rule.selectorText.replace(new RegExp(oldSelectorRegex, 'g'), newSelector);
+                    var selector = rule.selectorText.replace(new RegExp(selectorRegex, 'gi'), caseInsensitiveReplaceFunc);
 
                     updateRuleSelector(rule, selector);
                 });
@@ -62,25 +71,15 @@ var documentUtil = (function () {
         });
     };
 
+    module.rewriteStyleRuleSelector = function (doc, oldSelector, newSelector) {
+        replaceSimpleSelectorsBy(doc, [oldSelector], function () {
+            return newSelector;
+        });
+    };
+
     module.lowercaseTagNameSelectors = function (doc, matchingTagNames) {
-        var oldSelectorRegex = '(?:^|\\W)' + '(' + matchingTagNames.join('|') + ')' + '(?=\\W|$)';
-
-        asArray(doc.querySelectorAll('style')).forEach(function (styleElement) {
-            var matchingRules = asArray(styleElement.sheet.cssRules).filter(function (rule) {
-                return rule.selectorText && new RegExp(oldSelectorRegex, 'i').test(rule.selectorText);
-            });
-
-            if (matchingRules.length) {
-                matchingRules.forEach(function (rule) {
-                    var selector = rule.selectorText.replace(new RegExp(oldSelectorRegex, 'gi'), function (match) {
-                        return match.toLowerCase();
-                    });
-
-                    updateRuleSelector(rule, selector);
-                });
-
-                rewriteStyleContent(styleElement);
-            }
+        replaceSimpleSelectorsBy(doc, matchingTagNames, function (match) {
+            return match.toLowerCase();
         });
     };
 

test/specs/DocumentUtilSpec.js
@@ -80,6 +80,14 @@ describe("HTML Document Utility functions", function () {
             expect(doc.querySelector('style').textContent).toMatch(/a comment/);
         });
 
+        it("should match pseudo selector independent of letter case", function () {
+            setHtml('<style>a:HOver { color: blue; }/* a comment*/</style>');
+
+            documentUtil.rewriteStyleRuleSelector(doc, ':hover', '.myFakeHover');
+
+            expect(doc.querySelector('style').textContent).toMatch(/a.myFakeHover/);
+        });
+
         // On Firefox this needs a work around because of https://bugzilla.mozilla.org/show_bug.cgi?id=925493
         ifNotInPhantomJsIt("should integrate with a document loaded through ajax", function (done) {
             testHelper.readHTMLDocumentFixture("hover.html").then(function (doc) {

commit 23e0ea634003867f6296bfffdbfc78d8a4bbcced

Fix css selector parsing broken in cfa04ba1dc633ff4. Closes #86

Gruntfile.js
@@ -28,7 +28,8 @@ module.exports = function (grunt) {
                     'test/testHelper.js',
                     'test/gruntpath.js'
                 ],
-                display: 'short'
+                display: 'short',
+                summary: true
             }
         },
         browserify: {

src/documentUtil.js
@@ -43,11 +43,13 @@ var documentUtil = (function () {
 
     var matchingSimpleSelectorsRegex = function (simpleSelectorList) {
         return '(' +
-            '(?:^|\\W)' +                  // start of string or non-alphabetic character,
+            '(?:^|[^.#:\\w])' +            // start of string or not a simple selector character,
             '|' +                          // ... or ...
-            '(?=\\W)' +                    // the next character parsed is not an alphabetic character
+            '(?=\\W)' +                    // the next character parsed is not an alphabetic character (and thus a natural boundary)
             ')' +
+            '(' +
             simpleSelectorList.join('|') + // one out of the given simple selectors
+            ')' +
             '(?=\\W|$)';                   // followed either by a non-alphabetic character or the end of the string
     };
 

test/specs/DocumentUtilSpec.js
@@ -140,6 +140,22 @@ describe("HTML Document Utility functions", function () {
             expect(doc.querySelector('style').textContent).toMatch(/LI \{/);
         });
 
+        it("should not match any IDs with a matching alphabetical part", function () {
+            setHtml('<style>#A { color: blue; }</style>');
+
+            documentUtil.lowercaseCssTypeSelectors(doc, ['a']);
+
+            expect(doc.querySelector('style').textContent).toMatch(/#A \{/);
+        });
+
+        it("should not match a substring if the selector is in the middle of the tag list", function () {
+            setHtml('<style>#timeTaken { color: blue; }');
+
+            documentUtil.lowercaseCssTypeSelectors(doc, ['a', 'meta', 'i']);
+
+            expect(doc.querySelector('style').textContent).toMatch(/#timeTaken \{/);
+        });
+
         it("should convert complex selectors", function () {
             setHtml('<style>LI.COMPLEX:active { color: blue; }</style>');
 