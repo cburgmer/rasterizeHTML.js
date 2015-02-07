01e7fd4f20435c010fa0ed26b96563de7d5c5dc1
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-02-07 18:59:26 +0100

    Document issue in Chrome and Safari, uppercase class selectors break. #89

--- test/fixtures/testWithJs.js
+++ test/fixtures/testWithJs.js
@@ -2,7 +2,7 @@
     setTimeout(function () {
         if (window.matchMedia("(min-width: 200px) and (max-height: 100px)").matches) {
             var div = document.createElement("div");
-            div.className = "bgimage webfont test";
+            div.className = "bgimage webfont teST";
             div.innerHTML = "A";
             document.getElementsByTagName("body")[0].appendChild(div);
         }
