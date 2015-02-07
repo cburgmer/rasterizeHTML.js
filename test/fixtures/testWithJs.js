39cafc0c6e18d8dc4015368361fbcc7eb621737f
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-02-07 19:11:49 +0100

    Document integration test fixture

--- test/fixtures/testWithJs.js
+++ test/fixtures/testWithJs.js
@@ -1,3 +1,4 @@
+/* See testScaled50PercentWithJs.html for reference to integrational aspects of this file */
 (function () {
     setTimeout(function () {
         if (window.matchMedia("(min-width: 200px) and (max-height: 100px)").matches) {
